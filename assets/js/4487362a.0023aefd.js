a215c8d04786125752a453f2917a9f6b02ad8d04
hloo <17676976+hloo@users.noreply.github.com>
2021-04-03 00:08:54 -0700

    Deploy website - based on 8332e3f6a1486252ad983c87f785f91b2a257ce5

---
+++ assets/js/4487362a.0023aefd.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{85:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/ydot/blog/welcome"},{"title":"Hello","permalink":"/ydot/blog/hello-world"},{"title":"Hola","permalink":"/ydot/blog/hola"}]}')}}]);
---
+++ assets/js/afb657ed.efc3dc8c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[30],{101:function(o){o.exports=JSON.parse('{"allTagsPath":"/ydot/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/ydot/blog/tags/hello"}')}}]);
